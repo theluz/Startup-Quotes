cfc41a979e09c59d133bb54a2b4321433a881d4f
theluz <ti.rodrigoluz@gmail.com>
2019-07-16 16:39:04 -0300

    Inital commit with base files

---
+++ startup-quotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  startup-quotes
+//
+//  Created by Rodrigo Luz on 16/07/19.
+//  Copyright © 2019 Rodrigo Luz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
