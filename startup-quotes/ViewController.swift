9a58ef737e2014df3d3105a1d8f918dfc795eb83
theluz <ti.rodrigoluz@gmail.com>
2019-07-18 17:37:20 -0300

    Added an overlay between the bg and label to create a 'alpha transition' when calling the randomize function

--- startup-quotes/ViewController.swift
+++ startup-quotes/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var quoteBackground: UIImageView!
+    @IBOutlet weak var imageLayer: UIImageView!
     
-    let quotesArray = ["We are still Pre-Revenue...","We are coming out of Beta very soon...", "I’m not worried about losses because we are still in member acquisition mode...", "I know it is a kiosk deal, But...", "We are only taking money from friends and family at this time...", "Exactly when can I sell my shares...", "I will never sell out...", "I know it's my family member, but he/she is really the best for this position...","What is an org chart?", "We can have our company financials audited quickly and easily...", "We are loyalty based...", "We are currently speaking to (insert big name here)", "We tested off the charts...", "Our studies show (outlandish claim)", "The Media Loves us...", "We are developing an Ipad app as we speak...", "It is 100% scalable...", "We can bring this technology into any Industry...", "We will be on a panel at SXSW and CES next year...", "Our Company Culture is...", "We are committed to a supportive work environment...", "We are focused on many things...", "We can be everything to everyone", "We already have A LOT of Likes on Social Media...", "We are about to roll out our Social Media Campaign...", "Did I mention he / she went to Wharton?"]
+    let quotesArray = ["We are still Pre-Revenue...","We are coming out of Beta very soon...", "I’m not worried about losses because we are still in member acquisition mode...", "I know it is a kiosk deal, But...", "We are only taking money from friends and family at this time...", "Exactly when can I sell my shares...", "I will never sell out...", "I know it's my family member, but he/she is really the best for this position...","What is an org chart?", "We can have our company financials audited quickly and easily...", "We are loyalty based...", "We are currently speaking to (insert big name here)", "We tested off the charts...", "Our studies show (outlandish claim)", "The Media Loves us...", "We are developing an Ipad app as we speak...", "It is 100% scalable...", "We can bring this technology into any Industry...", "We will be on a panel at SXSW and CES next year...", "Our Company Culture is...", "We are committed to a supportive work environment...", "We are focused on many things...", "We can be everything to everyone...", "We already have A LOT of Likes on Social Media...", "We are about to roll out our Social Media Campaign...", "Did I mention he / she went to Wharton?"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +22,28 @@ class ViewController: UIViewController {
         randomize()
     }
     func randomize(){
+//        UIImageView.animate(withDuration: 3, animations: quoteBackground.alpha)
+        UIImageView.animate(withDuration: 0.7, animations: {
+            self.imageLayer.alpha = 0.8
+            self.imageLayer.alpha = 0.3
+            self.quoteLabel.alpha = 0.8
+            self.quoteLabel.alpha = 1
+            
+        })
+        
         let randomBackground = Int.random(in: 1...31)
         quoteBackground.image = UIImage(named:"bg" + "\(randomBackground)")
         
         let randomQuote = Int.random(in: 0...25)
         quoteLabel.text = quotesArray[randomQuote]
+        
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        randomize()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         randomize()
     }
     
